9ba77965e02d2cf80743d65b9236a019e2b46129
Seu Martins <Baielsantana@gmail.com>
2026-05-14 09:53:45 -0300

    Atualizado para meu amigo

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,88 +1,92 @@
 import { useState } from "react";
-import InputField from "./inputField"; 
+import InputField from "./inputField";
 import Botao from "./BotaoEnviar";
 
 function FormularioCadastro() {
-  const [nome, setnome] = useState('');
-  const [email, setemail] = useState('');
-  const [telefone, settelefone] = useState('');
-  const [erro, setErro] = useState('');
-  const [sucesso, setSucesso] = useState(false);
-  
-
+  // const [nome, setnome] = useState('');
+  // const [email, setemail] = useState('');
+  // const [telefone, settelefone] = useState('');
+  // const [erro, setErro] = useState('');
+  // const [sucesso, setSucesso] = useState(false);
+  const [user, setUser] = useState({ nome: "", email: "", telefone: "" });
+  const [erroSucesso, setErroSucesso] = useState({erro: "", sucesso:false})
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    if (nome.trim() === "") {
-      setErro("O campo nome não pode ser vazio")
+
+    if (user.nome.trim() === "") {
+      setErroSucesso("O campo nome não pode ser vazio")
       console.log(erro)
       return
     }
 
-    
-    setErro('');
-    setSucesso(true);
-    console.log({ nome, email, telefone }); //envio para o banco
-    setnome('')
-    setemail('')
-    settelefone('')
+    if (user.telefone.length !== 11) {
+      setErroSucesso("O campo telefone precisa de 11 digitos")
+      console.log(erro)
+      return
+    }
+
+
+
+    setErroSucesso('');
+    setErroSucesso({Sucesso: true});
+    console.log (user)
+    setUser({ nome: "", email: "", telefone: "" })  //envio para o banco
+
+
   };
 
-  const Telefone = (e) => {
-    e.preventDefault();
-  
 
-    if (telefone != length(11) ) {
-      setErro("O campo telefone precisa de 11 digitos")
-      console.log(erro)
-      return
-    }
-  
-
-    
-}
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        {erro && <p style={{ color: 'red'}}>{erro}</p>}
-        {sucesso && <p style = {{ color: 'green'}}> {"cadastrado com sucesso!"}</p>}
-         
+        {useState ({erro}) && <p style={{ color: 'red' }}>{useState ({erro}) }</p>}
+        {useState ({sucesso})  && <p style={{ color: 'green' }}> {"cadastrado com sucesso!"}</p>}
+
         <InputField
-          label="Nome" 
-          type="text" 
-          name="nome" 
-          placeholder="Gabriel..." 
-          value={nome} 
-          onChange={(e) => setnome(e.target.value)} 
+          label="Nome"
+          type="text"
+          name="nome"
+          placeholder="Gabriel..."
+          value={user.nome}
+          onChange={(e) => setUser(dados => ({
+            ...dados,
+            nome: e.target.value
+          }))}
         />
-        
+
         <div>
-          <InputField 
-            label="Telefone" 
-            type="tel" 
-            name="telefone" 
-            placeholder="+55..." 
-            value={telefone} 
-            onChange={(e) => settelefone(e.target.value)} 
+          <InputField
+            label="Telefone"
+            type="tel"
+            name="telefone"
+            placeholder="+55..."
+            value={user.telefone}
+            onChange={(e) => setUser(dados => ({
+            ...dados,
+            telefone: e.target.value
+          }))}
           />
         </div>
 
-        <InputField 
-          label="E-mail" 
-          type="email" 
-          name="email" 
-          placeholder="exemplo@email.com" 
-          value={email}
-          onChange={(e) => setemail(e.target.value)}
+        <InputField
+          label="E-mail"
+          type="email"
+          name="email"
+          placeholder="exemplo@email.com"
+          value={user.email}
+          onChange={(e) => setUser(dados => ({
+            ...dados,
+            email: e.target.value
+          }))}
         />
-        
+
         <InputField label="Nome da mae" type="text" name="NomeMae" placeholder="Mãe..." />
-        
+
         <Botao texto="Cadastrar" />
       </form>
 
-      <div>nome: {nome}</div>
-      <div>telefone: {telefone}</div>
+      <div>nome: {user.nome}</div>
+      <div>telefone: {user.telefone}</div>
     </div>
   );
 }
