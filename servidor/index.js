9bb3593e5848ceda017750ee84c4fc84788e840e
ninja1184 <guiguicraft734@gmail.com>
2026-05-15 10:53:32 -0300

    feat: servidor expres com rotas get e post

--- servidor/index.js
+++ servidor/index.js
@@ -1,2 +1,32 @@
-console.log("Hello, wolrd")
-console.log
+import express from 'express';
+const servidor = express()
+servidor.use(express.json())
+
+const registros = []
+
+servidor.post('/registros', (req, res) => {
+    const dados = req.body
+
+    if (!dados.nome) {
+        res.status(400).json({
+            erro: "campo de nome é obrigatorio"
+        })
+    }
+
+    console.log(`dados:
+        ${dados}`)
+    registros.push(dados)
+
+    res.status(201).json({
+        mensagem: "regisrtro criado",
+        sucesso: true,
+    })
+})
+
+servidor.get('/registros', (req, res) => {
+    res.status(200).json(registros)
+})
+
+servidor.listen(3000, () => {
+    console.log("o app esta rodando em http://localhost:3000")
+})
